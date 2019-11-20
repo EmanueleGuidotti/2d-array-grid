48f578865dc4413dbd2f653fae111c11625eec9c
emanuele.guidotti <guidotti.ema@gmail.com>
2019-11-20 12:14:00 +0100

    added style

--- src/containers/main.container.tsx
+++ src/containers/main.container.tsx
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import GridComponent from "../components/grid.comp";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 class MainContainer extends Component {
   render() {
     return (
       <View>
-        <Text>Title</Text>
+        <Text style={styles.h1}>Title</Text>
         <GridComponent></GridComponent>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  h1: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingVertical: 10
+  }
+});
 
 export default MainContainer;
